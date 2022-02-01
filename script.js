ccd245f7878b8eb43b446dca94113f50a83f3f23
Mitchell Nodland <mitch_nod@yahoo.com>
2022-02-01 15:52:26 -0500

    Update

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ var coin = 0.00;
 var minutes = 0;
 
 function start(){
+    //reset any previous values
+    coin = 0.00;
+    minutes = 0;
+
     //sets variables
     var hours = document.getElementById('hours').value;
     var menu = document.getElementById('settings');
-    
+    var pay = document.getElementById('pay').value;
+
     //toggles form
     menu.style.display = menu.style.display === 'none' ? '' : 'none';
     document.getElementById('menu').style.backgroundColor = "black";
@@ -14,7 +19,7 @@ function start(){
     //starts counting based on pay input
     var interval = setInterval(() => {
         minutes += 1;
-        coin += 0.20;
+        coin += (pay/60);
         document.getElementById('earnings').innerText = '$ ' + coin.toFixed(2);
         
         //stops counting based on hour input
@@ -28,6 +33,7 @@ function start(){
 function toggle_display(){
     var menu = document.getElementById('settings');
     menu.style.display = menu.style.display === 'none' ? '' : 'none';
+
     if (menu.style.display != 'none'){
         document.getElementById('menu').style.backgroundColor = "rgb(27, 27, 27)"
     }
